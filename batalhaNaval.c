e8603b203862f43d9fc3bad44cd49a335adedd67
Paulorr46 <155102702+Paulorr46@users.noreply.github.com>
2026-04-10 03:29:15 +0000

    alinhando colunas e linhas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,24 +2,22 @@
  
 int main() {
        int tabuleiro[10][10] = {0};
-       int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-       char linha[10] = {'A', 'B','C','D', 'E', 'F', 'G', 'H', 'I', 'J'};
+       int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       char coluna[10] = {'A', 'B','C','D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
 
-       printf("TABULEIRO BATALHA NAVAL \n");   
-        for(int j = 0; j < 1; j++){//pula uma linha entre a "linha"
+       printf("TABULEIRO BATALHA NAVAL \n");  
+       printf("   ");//espaço para alinha as colunas de A - J
             for(int i = 0; i < 10; i++){//imprimi a linha de 'A' a 'J'
-                printf("%c ", linha[i]);
+                printf("%c ", coluna[i]);
            }
                 printf("\n");//pula uma linha
-           }
-
-
+           //inicio da lógica para as linhas
             for(int i = 0; i < 10; i++){//pula linha entre colunas
-                printf("%d ", coluna[i]);
+                printf("%2d ", linha[i]);//garante que os numeros de 1 digito de cada linha ocupem espaço igual ao 10 para alinhar tudo;
                
             for(int j = 0; j < 10; j++){
-                printf("%d ", tabuleiro[i][j]);//imprimi as colunas
+                printf("%d ", tabuleiro[i][j]);//imprimi as linhas iniciadas com 0
                 }
                 printf("\n");
             }
