b7d6c48f7f37f182cf583a7f7bad1eafe5a04a94
Paulorr46 <155102702+Paulorr46@users.noreply.github.com>
2026-04-10 03:40:59 +0000

    colocando barcos representados pelo numeral 3 na matriz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,14 @@ int main() {
        int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
        char coluna[10] = {'A', 'B','C','D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
+        tabuleiro[1][4] = 3;
+        tabuleiro[1][5] = 3;
+        tabuleiro[1][6] = 3;
+
+        tabuleiro[3][3] = 3;
+        tabuleiro[4][3] = 3;
+        tabuleiro[5][3] = 3;
+
 
        printf("TABULEIRO BATALHA NAVAL \n");  
        printf("   ");//espaço para alinha as colunas de A - J
@@ -14,7 +22,7 @@ int main() {
                 printf("\n");//pula uma linha
            //inicio da lógica para as linhas
             for(int i = 0; i < 10; i++){//pula linha entre colunas
-                printf("%2d ", linha[i]);//garante que os numeros de 1 digito de cada linha ocupem espaço igual ao 10 para alinhar tudo;
+                printf("%2d ", linha[i]);//%2d garante que os numeros de 1 digito de cada linha ocupem espaço igual ao 10 para alinhar tudo;
                
             for(int j = 0; j < 10; j++){
                 printf("%d ", tabuleiro[i][j]);//imprimi as linhas iniciadas com 0
