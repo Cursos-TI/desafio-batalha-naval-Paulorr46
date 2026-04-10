9486687e28bb38d1cef208528f4a2ab6196ffafe
Paulorr46 <155102702+Paulorr46@users.noreply.github.com>
2026-04-10 17:23:19 +0000

    correções de comentários

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -22,8 +22,8 @@ int main() {
             
     //inicio da lógica do jogo
        printf("TABULEIRO BATALHA NAVAL \n");
-       printf("   ");//espaço para alinha as colunas de A - J
-            for(int i = 0; i < 10; i++){//imprimi a linha de 'A' a 'J'
+       printf("   ");//espaço para alinhar as colunas de A - J
+            for(int i = 0; i < 10; i++){//imprimi as colunas de 'A' a 'J'
                 printf("%c ", coluna[i]);
            }
                 printf("\n");//pula uma linha
@@ -32,7 +32,7 @@ int main() {
                 printf("%2d ", linha[i]);//%2d garante que os numeros de 1 digito de cada linha ocupem espaço igual ao 10 para alinhar tudo;
                
             for(int j = 0; j < 10; j++){
-                printf("%d ", tabuleiro[i][j]);//imprimi as linhas iniciadas com 0
+                printf("%d ", tabuleiro[i][j]);//imprimi o tabuleiro inteiro
                 }
                 printf("\n");
             }
